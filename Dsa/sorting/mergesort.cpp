1583e274e10180dc5f58fe30260b3b872ceb8deb
Manan Kuraware <manankuraware2017@gmail.com>
2026-08-11 23:41:31 +0530

    mergesort

---
+++ Dsa/sorting/mergesort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;      // to traverse first part of partion
+    int right = mid + 1; // to travese other half of partition
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+void mergeSort(int arr[], int low, int high)
+{
+    if (low == high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+int main()
+{
+    int arr[] = {22, 11, 4, 5, 6, 99, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
